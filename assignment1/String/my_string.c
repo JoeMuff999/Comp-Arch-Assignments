332266aaff860490531984ef66372e50191fb126
Joey <joeymuffoletto10@gmail.com>
2020-02-07 20:40:22 -0600

    made progress

---
+++ assignment1/String/my_string.c
@@ -0,0 +1,28 @@
+#include "my_string.h"
+
+
+size_t my_strlen ( const char * str ) 
+{
+    //your code goes here
+    return 0;
+}
+
+char * my_strcpy ( char * destination, const char * source )
+{
+    //your code goes here
+    return 0;
+}
+
+
+char * my_strncpy ( char * destination, const char * source, size_t num)
+{
+    //your code goes here
+    return 0;
+}
+
+void* my_memmove (void* destination, const void* source, size_t num)
+{
+    //your code goes here
+    return 0;
+}
+
